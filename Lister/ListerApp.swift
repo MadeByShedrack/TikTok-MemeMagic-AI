c0c8f86a6080cdd06c6369531f2645f679d128d9
Shedrack <madebyshedrack@gmail.com>
2023-11-25 16:00:22 +0100

    Initial Commit

---
+++ Lister/ListerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListerApp.swift
+//  Lister
+//
+//  Created by OgdenMorrow on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ListerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
